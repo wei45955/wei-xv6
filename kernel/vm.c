a01c5b2379eb59fc21012fdf3f1e4923355487c2
wei45955 <wei45955@hotmail.com>
2022-10-26 18:42:00 +0800

    add the vmprint function in vm.c

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint_helper(pagetable_t pagetable, int level)
+{
+  if(level > 3)
+    return;
+  for (int i = 0; i < 512; ++i)
+  {
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V) {
+      printf("..");
+      for (int it = 1; it < level; ++it) {
+        printf(" ..");
+      }
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      pagetable_t child = (pagetable_t)PTE2PA(pte);      
+      vmprint_helper(child, level + 1);
+    }
+  }
+}
+
+// 
+void vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprint_helper(pagetable, 1);
+}
